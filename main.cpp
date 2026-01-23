a13eeea67cbc6631c6afb5cb14181c434c525649
pkyle_sisyphus <kyle.park@ultradent.com>
2026-01-23 08:49:36 -0700

    [Documentation](sig_atomic_t vs std::atomic<bool>): Added comment clarifying use of std::atomic. I am fairly convince about this now

--- main.cpp
+++ main.cpp
@@ -3,11 +3,16 @@
 
 #include "structure_prototype.h"
 
+namespace {
+// Looking into sig_atomic_t vs std::atomic
+// Use std::atomic for signal + potential inter-thread shutdown signaling.
+// sig_atomic_t is only guaranteed safe w.r.t. signal interruption, not threads.
 std::atomic<bool> g_quit = false;
 static_assert(std::atomic<bool>::is_always_lock_free);
 void SignalHandler(int) {
   g_quit.store(true, std::memory_order_relaxed);
 }
+}  // namespace
 
 int main() {
   std::signal(SIGINT, SignalHandler);
